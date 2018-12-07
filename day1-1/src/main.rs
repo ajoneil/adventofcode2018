e92cd9b3f52ba60491501bb22c81380fa95fa2a3
Andrew O'Neil <andy@andyofniall.net>
2018-12-07 22:26:04 +1100

    Day 1-1

--- day1-1/src/main.rs
+++ day1-1/src/main.rs
@@ -1,3 +1,9 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    let numbers = buffer.split_whitespace().map(|num| -> i32 { num.parse().unwrap() });
+    let sum: i32 = numbers.sum();
+    println!("Sum: {}", sum);
 }
